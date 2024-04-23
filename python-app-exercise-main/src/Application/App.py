6b3006d4b5dc03bdb9a587ea13eaee7f33839f5b
formacionedmico <167799254+formacionedmico@users.noreply.github.com>
2024-04-23 11:41:39 +0200

    Delete python-app-exercise-main directory

--- python-app-exercise-main/src/Application/App.py
+++
@@ -1,9 +0,0 @@
-from src.Services.ApiService import ApiService
-
-
-class App:
-    def __init__(self):
-        self._api_service = ApiService()
-
-    def api_service(self) -> ApiService:
-        return self._api_service
